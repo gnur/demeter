f59e26e9db0e448655aa39f24457046ae5629759
Erwin de Keijzer <erwin@dekeijzer.xyz>
2018-12-19 22:21:59 +0100

    Update scrape description

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -21,25 +21,10 @@ import (
 // scrapeCmd represents the scrape command
 var scrapeCmd = &cobra.Command{
 	Use:   "scrape",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "all scrape related commands",
 }
 
 func init() {
 	rootCmd.AddCommand(scrapeCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// scrapeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// scrapeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
